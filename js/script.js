f7d87aab8dc830f52786e25f2a791c97326d1704
Maciek Bulski <fragmast3r@gmail.com>
2018-05-07 18:06:11 +0100

    fixed errors in script.js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@ var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&fi
 
 function getQuote() {
     $.getJSON(prefix + quoteUrl, createTweet);
-	$.ajaxSetup({ cache: false });
 }
+
+$.ajaxSetup({ cache: false });
 
 function createTweet(input) {
     var data = input[0];
@@ -30,7 +31,5 @@ function createTweet(input) {
 
 $(document).ready(function() {
     getQuote();
-    $('.trigger').click(function() {
-        getQuote();
-    })
+    $('.trigger').click(getQuote);
 });
